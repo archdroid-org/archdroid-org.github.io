eaa5ce3fecc6ca4d97af26f3aa3b2f3eaf8fc586
jgmdev <jgmdev@gmail.com>
2021-03-08 23:41:34 -0400

    Updates to create_package_pages.sh
    * Now supports deleting packages that are not listed on the
      packages.json file.
    * Fixed package generation issue where package names where also
      including ': '
    * Return successful status code when packages where removed or created
      and non successful if nothing changed.

--- create_package_pages.sh
+++ create_package_pages.sh
@@ -2,8 +2,25 @@
 
 set -e
 
-name="\"name\":"
+changes=1
 
+echo "Checking for packages that have been removed..."
+while read -r package
+do
+	if [ "$package" = "" ]; then
+		continue
+	fi
+	if ! grep "\"name\": \"$package\"" _data/packages.json > /dev/null ; then
+		echo "  Removing $package."
+		changes=0
+		git rm -r packages/"$package" > /dev/null 2>&1
+		rm -rf packages/"$package"
+	fi
+done < <(find packages -maxdepth 1 -type d | tail -n +2 | cut -d"/" -f2)
+
+name="\"name\": "
+
+echo "Checking for new packages..."
 while IFS="" read -r line || [ -n "${line}" ]
 do
 	if [[ ${line} == *${name}* ]]; then
@@ -13,11 +30,16 @@ do
 		index="${dir}/index.md"
 		[[ -d ${dir} ]] || mkdir -p ${dir}
 		if [[ ! -f "${index}" ]]; then
+			echo "  Adding $pkgname."
 			cat <<EOF >${index}
 ---
 layout: "archdroid/package"
 ---
 EOF
+			changes=0
 		fi
 	fi
 done < _data/packages.json
+
+# return false if no changes occurred.
+exit $changes
