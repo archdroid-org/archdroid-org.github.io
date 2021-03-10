abae738c1b719c7ff8c60b9643aaf56db7e1745e
jgmdev <jgmdev@gmail.com>
2021-03-10 05:44:09 -0400

    Implemented base functionality of translation helper script.

--- _tools/translation.sh
+++ _tools/translation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# get value of an argument option, usage:
-# alarm_getopt <arg_name> <multiple_values?1|0> <string>
-# eg: alarm_getopt packages 1 $@
+# Get value of an argument option, usage:
+# util_getopt <arg_name> <multiple_values?1|0> <string>
+# eg: util_getopt packages 1 $@
 util_getopt(){
 	local option_single="-$1"
 	local option_double="--$1"
@@ -34,6 +34,25 @@ util_getopt(){
 
 		shift
 	done
+}
+
+# Check if a command line flag is set, usage:
+# util_flagset <flag-short-name> <flag-long-name> <arguments-to-check>
+# eg: util_getopt i interactive $@
+util_flagset(){
+	local option_single="-$1"
+	local option_double="--$2"
+
+	while [ "$3" ]; do
+		case "$3" in
+			"$option_single" | "$option_double" )
+					return 0
+					;;
+			esac
+		shift
+	done
+
+	return 1
 }
 
 # Converts a yaml file into a key=value pairs for easy bash parsing.
@@ -69,17 +88,180 @@ function util_parse_yaml_get(){
 		local name=$(echo "$line" | cut -d"=" -f1)
 		if [ "$name" = "$2" ]; then
 			echo "$line" | cut -d"=" -f2 | sed 's/^"//;s/"$//'
-			return
+			return 0
 		fi
 	done < <(util_parse_yaml $1)
+
+	return 1
 }
 
 add_language(){
+	if [ "$1" = "" ] || [ "$2" = "" ]; then
+		echo "Please provide a language code and language name, eg:"
+		echo ""
+		echo "  site translation add es Spanish"
+		return 1
+	fi
+	local lang_code="$1"
+	local lang_name="$2"
+
+	if [ -e "./_data/locale/$lang_code" ]; then
+		echo "Error: the language already exists."
+		return 1
+	fi
+
+	local default_lang=$(util_parse_yaml_get _config.yml locale_default)
+
+	if [ ! -e _data/locale/"$default_lang" ]; then
+		echo "Error: the default language doesn't exists."
+		return 1
+	fi
+
+	cp -a _data/locale/"$default_lang" _data/locale/"$lang_code"
+
+	sed -i 's/^name: *"\(.*\)"/name: "'"$lang_name"'"/' \
+		_data/locale/"$lang_code"/translation.yml
+
+	if [ ! -e "$lang_code" ]; then
+		mkdir "$lang_code"
+		cp "$default_lang/index.md" "$lang_code/"
+	fi
+
+	echo "Language successfully added!"
 	echo ""
+	echo "You can start by translating the files:"
+	echo "  _data/locale/$lang_code/layout.yml"
+	echo "  _data/locale/$lang_code/translation.yml"
+	echo "  $lang_code/index.md"
+	echo ""
+	echo "Then you can run 'site translation check $lang_code'"
+	echo "to see the content that needs to be translated or"
+	echo "'site translation check $lang_code --interactive'"
+	echo "in order to interactively translate the content"
+	echo "while it is been checked up."
+	echo ""
+	echo "Make sure to set the EDITOR environment variable to point"
+	echo "to the editor that you will be using for the editing work."
+
+	return 0
+}
+
+delete_language(){
+	if [ "$1" = "" ]; then
+		echo "Please provide a language code, eg:"
+		echo ""
+		echo "  site translation rm es"
+		return 1
+	fi
+
+	local lang_code="$1"
+
+	if [ ! -e "./_data/locale/$lang_code" ]; then
+		echo "Error: the language '$lang_code' does not exists."
+		return 1
+	fi
+
+	echo "This process will remove the entire language from the site."
+	echo -n "Are you sure you want to continue? [y/N]: "
+	local answer=""
+	read answer
+	case $answer in
+		'y' | 'Y' )
+			rm -rf ./_data/locale/"$lang_code"
+			rm -rf "$lang_code"
+			echo "Language '$lang_code' removed."
+			return 0
+			;;
+	esac
+
+	return 1
 }
 
 check_language(){
-	echo ""
+	local lang_code="$1"
+	local languages=$(list_languages | cut -d":" -f1)
+	local interactive="0"
+
+	if [ "$lang_code" = "" ] || ! echo "$languages" | grep "$lang_code" > /dev/null; then
+		echo "The language code you gave doesn't exists."
+		return 1
+	fi
+
+	if util_flagset i interactive $@ ; then
+		interactive="1"
+
+		if [ "$EDITOR" = "" ] || ! which "$EDITOR" > /dev/null; then
+			echo "Error: please set a text editor command in EDITOR envar."
+			return 1
+		fi
+	fi
+
+	local answer=""
+
+	echo "Checking status of translations..."
+	while read -r dir <&3; do
+		if ! echo "$languages" | grep "$dir" > /dev/null; then
+			if [ "$interactive" = "1" ]; then
+				echo "  Found directory '$dir' what do you want to do?"
+				echo -n "  (c)heck, (s)kip [c/S]: "
+				read answer
+				case $answer in
+					'c' | 'C' )
+						echo "  Checking '$dir'..."
+						;;
+					* )
+						continue
+						;;
+				esac
+			fi
+			while read -r file <&4; do
+				if [ ! -e "$lang_code/$file" ]; then
+					echo "  Missing translation for '$file'"
+					if [ "$interactive" = "1" ]; then
+						echo "    What do you want to do?"
+						echo -n "    (c)opy, (k)copy and edit, (s)kip [c/k/S]: "
+						read answer
+						case $answer in
+							'c' | 'C' )
+								local dirpart=$(dirname $file)
+								mkdir -p "$lang_code/$dirpart"
+								cp -a "$file" "$lang_code/$dirpart"
+								;;
+							'k' | 'K' )
+								local dirpart=$(dirname $file)
+								mkdir -p "$lang_code/$dirpart"
+								cp -a "$file" "$lang_code/$dirpart"
+								$EDITOR "$lang_code/$file"
+								;;
+						esac
+					fi
+				else
+					local current_md5=$(md5sum "$file" | cut -d" " -f1)
+					local translation_md5=$(md5sum "$lang_code/$file" | cut -d" " -f1)
+					if [ "$current_md5" = "$translation_md5" ]; then
+						echo "  Warning, '$lang_code/$file' hasn't been translated"
+						if [ "$interactive" = "1" ]; then
+							echo "    What do you want to do?"
+							echo -n "    (e)dit, (s)kip [e/S]: "
+							read answer
+							case $answer in
+								'e' | 'E' )
+									$EDITOR "$lang_code/$file"
+									;;
+							esac
+						fi
+					fi
+				fi
+			done 4< <(find "$dir" -name "*.md")
+		fi
+	done 3< <(
+		find . -maxdepth 1 -type d \
+			| sed 's/^\.\///' \
+			| grep -Ev "^_|.git|^images|^assets|^node_modules" \
+			| tail -n+2
+	)
+
+	return 0
 }
 
 list_languages(){
@@ -98,26 +280,100 @@ list_languages(){
 	done
 }
 
+sync_language(){
+	local lang_code="$1"
+	local languages=$(list_languages | cut -d":" -f1)
+	local interactive="0"
+
+	if [ "$lang_code" = "" ] || ! echo "$languages" | grep "$lang_code" > /dev/null; then
+		echo "The language code you gave doesn't exists."
+		return 1
+	fi
+
+	if util_flagset i interactive $@ ; then
+		interactive="1"
+	fi
+
+	local answer=""
+
+	echo "Syncing '$lang_code' with default translation files..."
+	while read -r dir <&3; do
+		if ! echo "$languages" | grep "$dir" > /dev/null; then
+			if [ "$interactive" = "1" ]; then
+				echo "  Found directory '$dir' what do you want to do?"
+				echo -n "  (c)heck, (s)kip [c/S]: "
+				read answer
+				case $answer in
+					'c' | 'C' )
+						echo "  Checking '$dir'..."
+						;;
+					* )
+						continue
+						;;
+				esac
+			fi
+			while read -r file <&4; do
+				if [ ! -e "$lang_code/$file" ]; then
+					if [ "$interactive" = "1" ]; then
+						echo "    What do you want to do?"
+						echo -n "    (c)opy, (s)kip [c/S]: "
+						read answer
+						case $answer in
+							'c' | 'C' )
+								local dirpart=$(dirname $file)
+								mkdir -p "$lang_code/$dirpart"
+								cp -a "$file" "$lang_code/$dirpart"
+								;;
+						esac
+					else
+						echo "  Copying '$file' to '$lang_code'."
+						local dirpart=$(dirname $file)
+						mkdir -p "$lang_code/$dirpart"
+						cp -a "$file" "$lang_code/$dirpart"
+					fi
+				fi
+			done 4< <(find "$dir" -name "*.md")
+		fi
+	done 3< <(
+		find . -maxdepth 1 -type d \
+			| sed 's/^\.\///' \
+			| grep -Ev "^_|.git|^images|^assets|^node_modules" \
+			| tail -n+2
+	)
+
+	return 0
+}
+
 case $1 in
 	'add' )
     shift
     add_language $@
-    exit
+    exit $?
+    ;;
+	'rm' )
+    shift
+    delete_language $@
+    exit $?
     ;;
   'check' )
     shift
     check_language $@
-    exit
+    exit $?
     ;;
   'list' )
     shift
     list_languages $@
-    exit
+    exit $?
+    ;;
+	'sync' )
+    shift
+    sync_language $@
+    exit $?
     ;;
 	'yaml-get' )
 		shift
 		util_parse_yaml_get $1 $2
-		exit
+		exit $?
 		;;
   * )
     script_name=$(basename $0 | sed "s/\.sh$//")
@@ -130,8 +386,16 @@ case $1 in
     echo "Available Commands:"
     echo ""
     echo -e "  ${g}add${d}     Add new language."
+		echo -e "          Params: <lang-code> <lang-name>"
+		echo -e "  ${g}rm${d}      Deletes a language."
+		echo -e "          Params: <lang-code>"
     echo -e "  ${g}check${d}   Verify the status of a language translation."
+		echo -e "          Params: <lang-code>"
+		echo -e "          Flags:  -i | --interactive"
     echo -e "  ${g}list${d}    List site translations."
+    echo -e "  ${g}sync${d}    Syncs a given language with system default."
+		echo -e "          Params: <lang-code>"
+		echo -e "          Flags:  -i | --interactive"
     echo -e "  ${g}help${d}    Print this help."
     exit
     ;;
