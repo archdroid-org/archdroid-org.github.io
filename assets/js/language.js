6a249ed415c2295df0002ce9bb99ab4e14cb6514
jgmdev <jgmdev@gmail.com>
2021-03-11 07:42:23 -0400

    Redirect when pages not translated and added translation support using javascript.

--- assets/js/language.js
+++ assets/js/language.js
@@ -4,16 +4,50 @@ if(!localStorage.getItem("language")){
 	localStorage.setItem("language", default_lang);
 }
 
+var page_loading=false;
+
 // When visiting the homepage redirect to user chosen language.
 var current_lang=localStorage.getItem("language");
 if(location.pathname == "/"){
 	if(current_lang != default_lang){
 		location.href="/" + current_lang + "/";
+		page_loading = true;
+	}
+}
+
+if(current_lang != default_lang){
+	$("[data-localize]").localize(
+		"",
+		{
+			language: current_lang,
+			pathPrefix: "/assets/localization"
+		}
+	);
+}
+
+// Redirect not found pages to default language or root path
+if($("#page-not-found").length > 0){
+	var path_parts=location.pathname.split("/");
+	if(path_parts[1] != default_lang){
+		if(path_parts[1] == current_lang){
+			path_parts[1] = default_lang;
+			location.href = path_parts.join("/");
+		} else{
+			location.href = "/" + default_lang + location.pathname;
+		}
+		page_loading = true;
+	} else{
+		path_parts.splice(1, 1);
+		location.href = path_parts.join("/");
+		page_loading = true;
 	}
 }
 
 // Register user language of choice on language menu click.
 $(document).ready(function(){
+	if(!page_loading)
+		$("body").css("visibility", "visible");
+
 	$("#language-menu a").click(function(){
 		localStorage.setItem("language", $(this).attr("data-lang"));
 	});
